63c855f04bcad790c0f48b6bde63279ccbf78803
Your Name <sebastian.bjelkstam@gmail.com>
2023-12-28 11:02:54 +0100

    Uppdate style

--- wordpress/wp-content/themes/mytheme/header.php
+++ wordpress/wp-content/themes/mytheme/header.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url') ?>">
+    <script src="https://kit.fontawesome.com/ed4823c162.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
@@ -13,12 +14,29 @@
 
     <?php get_header() ?>
 
-    <header>
-        <h1>
-            <a href="">
+    <header class="theHeader">
+        <h1 class="theHeadName">
+            <a href="http://localhost/clothingstore/wordpress/">
                 <?php bloginfo('name') ?>
             </a>
         </h1>
 
-        <?php wp_nav_menu() ?>
+        <i onclick="theOpenMenu()" class="fa-solid fa-bars"></i>
+
+
+        <div class="theHamburgerMenu">
+            <i onclick="theClose()" class="fa-solid fa-x"></i>
+            <?php wp_nav_menu() ?>
+        </div>
     </header>
+
+
+    <script>
+        function theOpenMenu() {
+            document.querySelector('.theHamburgerMenu').classList.toggle('youOpen')
+        }
+
+        function theClose() {
+            document.querySelector('.theHamburgerMenu').classList.remove('youOpen')
+        }
+    </script>
--- wordpress/wp-content/themes/mytheme/page.php
+++ wordpress/wp-content/themes/mytheme/page.php
@@ -11,6 +11,11 @@
 
     <?php get_header() ?>
 
+    <div class="backgroundStyleForImg">
+        <img class="theStartImg" src="<?php echo get_field('theimg')  ?>" alt="">
+
+    </div>
+
     <?php
     if (have_posts()) :
         while (have_posts()) : the_post();
@@ -20,6 +25,7 @@
     ?>
 
     <?php get_footer() ?>
+
 </body>
 
 </html>
