5c439de73f362d338d1588282ef27bcfcd3331e7
Your Name <sebastian.bjelkstam@gmail.com>
2023-12-29 11:18:13 +0100

    mytheme

--- wordpress/wp-content/themes/mytheme/header.php
+++ wordpress/wp-content/themes/mytheme/header.php
@@ -23,10 +23,35 @@
 
         <i onclick="theOpenMenu()" class="fa-solid fa-bars"></i>
 
+        <div class="theComputerNav">
+            <?php
+            wp_nav_menu()
+            ?>
+            <!-- <p class="theBtnForCategory2" onclick="openCategory2()">Kategory</p> -->
+            <!-- <div class="theCategoryDiv2"> -->
+            <!-- <p class="theCategoryUL"> -->
+            <!-- <li class="theLI"><a class="theA" href="">T-shirt</a></li>
+                <li class="theLI"><a class="theA" href="">Huvtröja</a></li>
+                <li class="theLI"><a class="theA" href="">Mjukisbyxa</a></li> -->
+            <!-- </p> -->
+            <!-- </div> -->
+
+        </div>
+
+
+        </div>
 
         <div class="theHamburgerMenu">
             <i onclick="theClose()" class="fa-solid fa-x"></i>
             <?php wp_nav_menu() ?>
+            <!-- <p class="theBtnForCategory" onclick="openCategory()">Kategory</p> -->
+            <!-- <div class="theCategoryDiv"> -->
+            <!-- <p class="theCategoryUL"> -->
+            <!-- <li class="theLI"><a class="theA" href="">T-shirt</a></li>
+                <li class="theLI"><a class="theA" href="">Huvtröja</a></li>
+                <li class="theLI"><a class="theA" href="">Mjukisbyxa</a></li> -->
+            <!-- </p> -->
+            <!-- </div> -->
         </div>
     </header>
 
@@ -38,5 +63,16 @@
 
         function theClose() {
             document.querySelector('.theHamburgerMenu').classList.remove('youOpen')
+            document.querySelector('.theCategoryDiv').classList.remove('openCategory')
+
+        }
+
+
+        function openCategory() {
+            document.querySelector('.theCategoryDiv').classList.toggle('openCategory')
+        }
+
+        function openCategory2() {
+            document.querySelector('.theCategoryDiv2').classList.toggle('openCategory2')
         }
     </script>
--- wordpress/wp-content/themes/mytheme/page.php
+++ wordpress/wp-content/themes/mytheme/page.php
@@ -11,15 +11,12 @@
 
     <?php get_header() ?>
 
-    <div class="backgroundStyleForImg">
-        <img class="theStartImg" src="<?php echo get_field('theimg')  ?>" alt="">
-
-    </div>
-
     <?php
     if (have_posts()) :
         while (have_posts()) : the_post();
             the_content();
+        // the_category();
+        // wp_list_categories();
         endwhile;
     endif;
     ?>
