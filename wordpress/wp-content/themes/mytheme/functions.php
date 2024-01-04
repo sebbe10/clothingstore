3638960b4bde9f754d06223f2989446ce372b12f
Your Name <sebastian.bjelkstam@gmail.com>
2024-01-04 18:28:51 +0100

    Comments

--- wordpress/wp-content/themes/mytheme/functions.php
+++ wordpress/wp-content/themes/mytheme/functions.php
@@ -4,3 +4,11 @@ function otherMenu()
     register_nav_menu('page-menu', __('My Custom Menu'));
 }
 add_action('init', 'otherMenu');
+?>
+
+<?php
+
+
+
+?>
+
--- wordpress/wp-content/themes/mytheme/single-product.php
+++ wordpress/wp-content/themes/mytheme/single-product.php
@@ -9,15 +9,22 @@
 
 <body>
 
-    <?php get_header() ?>
+    <?php
+
+    use Automattic\WooCommerce\Blocks\BlockTypes\ProductSKU;
+
+    get_header() ?>
 
     <?php
     if (have_posts()) :
         while (have_posts()) : the_post();
             the_content();
+
         endwhile;
     endif;
     ?>
+
+
 
     <?php get_footer() ?>
 </body>
