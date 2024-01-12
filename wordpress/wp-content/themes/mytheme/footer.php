89602ddcc841570aa7040c2a59c421604e9dbe57
Your Name <sebastian.bjelkstam@gmail.com>
2024-01-12 09:04:50 +0100

    Product

--- wordpress/wp-content/themes/mytheme/footer.php
+++ wordpress/wp-content/themes/mytheme/footer.php
@@ -1,6 +1,15 @@
 <footer class="theFooter">
     <!-- <p>Kontakta: 07222255556</p> -->
     <p>Mail: clothingStore@gmail.com</p>
+
+    <?php
+    if (get_the_title() !== 'Kontakta oss') {
+        wp_nav_menu(array(
+            'theme_location' => 'page-menu'
+
+        ));
+    }
+    ?>
 </footer>
 
 <?php get_footer() ?>
